63a9e4cc24ae234bfe2d8df7c3b391f719a8def3
MEGBLETO carnel <carnelilordwi@gmail.com>
2020-08-14 21:06:41 +0530

    second commit express server up and running

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require('express'); //bringing in express
 
-const app = express();
+const app = express(); //initializing our app with express
 
 app.get('/', (req, res) => res.send('API Running'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; //setting our port will look for anv variable and if there in none we use port 5000
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
